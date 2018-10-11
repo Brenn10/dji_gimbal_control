686bd040ccb8f710902beb6520e11c7557b4ffcc
MikeK4y <mike.kalaitzakis@gmail.com>
2018-10-11 17:10:45 -0400

    Added tag tracking using AR tags

--- include/dji_gimbal_control/dji_gimbal_control.h
+++ include/dji_gimbal_control/dji_gimbal_control.h
@@ -6,7 +6,9 @@
 
 // ROS includes
 #include <ros/ros.h>
+#include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Joy.h>
 
 #define C_PI (double)3.141592653589793
@@ -24,25 +26,36 @@ public:
 private:
 	// Subscribers
 	ros::Subscriber gimbalAngleSub;
-  ros::Subscriber joySub;
+	ros::Subscriber joySub;
+	ros::Subscriber cameraInfoSub;
+	ros::Subscriber tagPoseSub;
 
 	// Publishers
 	ros::Publisher gimbalSpeedPub;
-  ros::Publisher gimbalAnglePub;
+	ros::Publisher gimbalAnglePub;
 
 	// Callbacks
 	void gimbalAngleCallback(const geometry_msgs::Vector3Stamped::ConstPtr& msg);
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& msg);
+	void joyCallback(const sensor_msgs::Joy::ConstPtr& msg);
+	void cameraInfoCallback(const sensor_msgs::CameraInfo& msg);
+	void tagCallback(const ar_track_alvar_msgs::AlvarMarkers& msg);
 
-  // Functions
-  void initializeJoy();
-  void resetGimbalAngle();
-  void faceDownwards();
-  
+	// Functions
+	void initializeParam();
+	void resetGimbalAngle();
+	void faceDownwards();
+
 	// Data
-  int yawAxis, pitchAxis, rollAxis, resetButton, faceDownButton;
+	bool tagFound;
+	double fx, fy;
+	double velT, invKp;
+	int tagX, tagY;
+
+	bool trackTag;
+	std::string cameraInfoTopic;
+	int yawAxis, pitchAxis, rollAxis, resetButton, faceDownButton, toggleButton;
 	geometry_msgs::Vector3Stamped gimbalAngle;
-  geometry_msgs::Vector3Stamped speedCmd;
+	geometry_msgs::Vector3Stamped speedCmd;
 };
 
 #endif //DJI_GIMBAL_CONTROL_H
--- src/dji_gimbal_control.cpp
+++ src/dji_gimbal_control.cpp
@@ -2,43 +2,63 @@
 
 #include <unistd.h>
 
+#include <iostream>
+
 dji_gimbal_control::dji_gimbal_control(ros::NodeHandle& nh)
 {
-	// Setup the joystick axes
-	initializeJoy();
+	// Configure the gimbal control parameters
+	initializeParam();
 
 	// Setup Subscribers
-	gimbalAngleSub = nh.subscribe<geometry_msgs::Vector3Stamped>("dji_sdk/gimbal_angle", 10, &dji_gimbal_control::gimbalAngleCallback, this);
-	joySub = nh.subscribe("joy", 10, &dji_gimbal_control::joyCallback, this);
+	gimbalAngleSub = nh.subscribe<geometry_msgs::Vector3Stamped>("/dji_sdk/gimbal_angle", 10, &dji_gimbal_control::gimbalAngleCallback, this);
+	joySub = nh.subscribe("/joy", 10, &dji_gimbal_control::joyCallback, this);
+	cameraInfoSub = nh.subscribe(cameraInfoTopic, 10, &dji_gimbal_control::cameraInfoCallback, this);
+	tagPoseSub = nh.subscribe("/ar_pose_marker", 10, &dji_gimbal_control::tagCallback, this);
 
 	// Setup Publishers
-	gimbalSpeedPub = nh.advertise<geometry_msgs::Vector3Stamped>("dji_sdk/gimbal_speed_cmd", 10);
-	gimbalAnglePub = nh.advertise<dji_sdk::Gimbal>("dji_sdk/gimbal_angle_cmd", 10);
-
-	// Initialize speed command
-	speedCmd.vector.x = 0;
-	speedCmd.vector.y = 0;
-	speedCmd.vector.z = 0;
+	gimbalSpeedPub = nh.advertise<geometry_msgs::Vector3Stamped>("/dji_sdk/gimbal_speed_cmd", 10);
+	gimbalAnglePub = nh.advertise<dji_sdk::Gimbal>("/dji_sdk/gimbal_angle_cmd", 10);
 }
 
-void dji_gimbal_control::initializeJoy()
+void dji_gimbal_control::initializeParam()
 {
 	// Create private nodeHandle to read the launch file
 	ros::NodeHandle nh_private("~");
 
 	// Load values from launch file
+	nh_private.param("track_tag", trackTag, false);
+	nh_private.param("camera_info_topic", cameraInfoTopic, std::string("/dji_camera/camera_info"));
 	nh_private.param("yaw_axis", yawAxis, 0);
 	nh_private.param("pitch_axis", pitchAxis, 4);
 	nh_private.param("roll_axis", rollAxis, 3);
 	nh_private.param("reset_angle_btn", resetButton, 0);
 	nh_private.param("face_down_btn", faceDownButton, 2);
+	nh_private.param("toggle_track_btn", toggleButton, 3);
+
+	// Initialize speed command
+	speedCmd.vector.x = 0;
+	speedCmd.vector.y = 0;
+	speedCmd.vector.z = 0;
+
+	// Initialize flags
+	tagFound = false;
+	velT = 0.75;
+	invKp = 500.0;
 }
 
-void dji_gimbal_control::publishGimbalCmd() {
+void dji_gimbal_control::publishGimbalCmd()
+{
+	if (trackTag && tagFound)
+	{
+		speedCmd.vector.x = 0;
+		speedCmd.vector.y = -tagY / invKp > velT ? velT : -tagY / invKp < -velT ? -velT : -tagY / invKp;
+		speedCmd.vector.z = tagX / invKp > velT ? velT : tagX / invKp < -velT ? -velT : tagX / invKp;
+	}
 	gimbalSpeedPub.publish(speedCmd);
 }
 
-void dji_gimbal_control::resetGimbalAngle() {
+void dji_gimbal_control::resetGimbalAngle()
+{
 	// Prepare the reset command
 	dji_sdk::Gimbal angleCmd;
 	angleCmd.mode |= 0;
@@ -53,7 +73,8 @@ void dji_gimbal_control::resetGimbalAngle() {
 	sleep(2);
 }
 
-void dji_gimbal_control::faceDownwards() {
+void dji_gimbal_control::faceDownwards()
+{
 	// Prepare the angle command
 	dji_sdk::Gimbal angleCmd;
 	angleCmd.mode |= 0;
@@ -75,6 +96,10 @@ void dji_gimbal_control::gimbalAngleCallback(const geometry_msgs::Vector3Stamped
 
 void dji_gimbal_control::joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
+	// Toggle track flag
+	if (msg->buttons[toggleButton] == 1)
+		trackTag = !trackTag;
+
 	if (msg->buttons[resetButton] == 1)
 		resetGimbalAngle();
 	else if (msg->buttons[faceDownButton] == 1)
@@ -86,6 +111,32 @@ void dji_gimbal_control::joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 		speedCmd.vector.y = -msg->axes[pitchAxis];
 		speedCmd.vector.z = -msg->axes[yawAxis];
 	}
+}
+
+void dji_gimbal_control::cameraInfoCallback(const sensor_msgs::CameraInfo& msg)
+{
+	// Get the focal length of the camera
+	fx = msg.K[0];
+	fy = msg.K[4];
+}
+
+void dji_gimbal_control::tagCallback(const ar_track_alvar_msgs::AlvarMarkers& msg)
+{
+	if(!msg.markers.empty())
+	{
+		// Get Tag center position
+		double posTagX = msg.markers[0].pose.pose.position.x;
+		double posTagY = msg.markers[0].pose.pose.position.y;
+		double posTagZ = msg.markers[0].pose.pose.position.z;
+
+		// Get tag pose in image coordinates
+		tagX = fx*(posTagX/posTagZ);
+		tagY = fy*(posTagY/posTagZ);
+
+		tagFound = true;
+	}
+	else
+		tagFound = false;
 }
 
 ////////////////////////////////////////////////////////////
